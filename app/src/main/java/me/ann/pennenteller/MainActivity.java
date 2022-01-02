26945c542e1f5a786f94eb9af333d062f012bb9a
Anne-Marie <aknijffkomen@hotmail.com>
2022-01-02 14:30:16 +0100

    Toevoegen Add, Subtract, reset en undo

--- app/src/main/java/me/ann/pennenteller/MainActivity.java
+++ app/src/main/java/me/ann/pennenteller/MainActivity.java
@@ -2,31 +2,81 @@ package me.ann.pennenteller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private static int teller = 0;
+    private static final int MINIMUM = 0;
+
+    TextView tellerText;
+
+    int tellerBeforeUndo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tellerText = findViewById(R.id.teller_text);
+
+        tellerText = findViewById(R.id.teller_text);
         Button tellerAdd = findViewById(R.id.teller_add);
-        Button tellerSubtract = findViewById(R.id.teller_subtract);
+        Button tellerSub = findViewById(R.id.teller_sub);
+        Button reset = findViewById(R.id.reset);
+
+        setupLocalStorage();
 
         tellerAdd.setOnClickListener(view -> {
-            teller++;
-            tellerText.setText(String.valueOf(teller));
+            updateTeller(teller + 1);
         });
-        tellerSubtract.setOnClickListener(view -> {
-            teller --;
-            tellerText.setText(String.valueOf(teller));
+
+        tellerSub.setOnClickListener(view -> {
+            if (teller -1 < MINIMUM){
+                Snackbar mySnackbar = Snackbar.make(view, "De teller mag niet minder dan " + MINIMUM +" zijn.",  BaseTransientBottomBar.LENGTH_SHORT );
+                mySnackbar.show();
+                return;
+            }
+            updateTeller(teller - 1);
         });
+
+        reset.setOnClickListener(view -> {
+//            Op 0 zetten van teller, tenzij gekozen wordt voor Undo...
+            tellerBeforeUndo = teller;
+            updateTeller(0);
+            Snackbar mySnackbar = Snackbar.make(view, "Teller teruggezet", Snackbar.LENGTH_LONG);
+            mySnackbar.setAction("Undo", view1 -> {
+                updateTeller(tellerBeforeUndo);
+            });
+            mySnackbar.show();
+        });
+
+
+    }
+
+    private void setupLocalStorage() {
+        SharedPreferences sharedPreferences = getSharedPreferences("opslag", MODE_PRIVATE);
+        int uitOpslag = sharedPreferences.getInt("teller", 0);
+        teller = uitOpslag;
+        tellerText.setText(String.valueOf(teller));
+    }
+
+    private void updateTeller(int nieuweTeller) {
+        SharedPreferences sharedPreferences = getSharedPreferences("opslag", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putInt("teller", nieuweTeller);
+        editor.apply();
+        teller = nieuweTeller;
+        tellerText.setText(String.valueOf(teller));
+
     }
 
 }
